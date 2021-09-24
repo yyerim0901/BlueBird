8c8445de20b40522187ef3538e6c658c9ce31e8a
이용직 <yj_7679@naver.com>
2021-09-24 16:17:26 +0900

    astar다시 다듬고 완성

--- backend/controllers/DeviceController.js
+++ backend/controllers/DeviceController.js
@@ -19,6 +19,34 @@ module.exports = {
             'device_number' : req.body.device_number
         }
         
+        var fs = require('fs');
+        var openApiURL = 'http://aiopen.etri.re.kr:8000/WiseASR/Recognition';
+        var accessKey = '0daad576-d02b-434e-a5ea-3db5adf90b74';
+        var languageCode = 'korean';
+        var audioFilePath = 'C:\\Users\\multicampus\\Downloads\\KOR_F_RM0769FLJH0325\\KOR_F_RM0769FLJH0325.pcm';
+        var audioData;
+
+        var audioData = fs.readFileSync(audioFilePath);
+
+        var requestJson = {
+            'access_key': accessKey,
+            'argument': {
+                'language_code': languageCode,
+                'audio': audioData.toString('base64')
+            }
+        };
+
+        var request = require('request');
+        var options = {
+            url: openApiURL,
+            body: JSON.stringify(requestJson),
+            headers: {'Content-Type':'application/json; charset=UTF-8'}
+        };
+        request.post(options, function (error, response, body) {
+            console.log('responseCode = ' + response.statusCode);
+            console.log('responseBody = ' + body);
+        });
+        
         console.log('turn on #device : ', req.body.device_number);
         Device.onDevice(data).then((result)=>{
             res.send(result);
