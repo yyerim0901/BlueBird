0a6beec6357fc4b35a61f9cafd31a685ac2b4b6e
김예림 <bam03196@naver.com>
2021-10-01 14:54:24 +0900

    add notification

--- front/src/views/Footer.vue
+++ front/src/views/Footer.vue
@@ -1,24 +1,48 @@
 <template>
     <div class="ft_set container">
         <div class="row">
-            <div class="col">
+            <div class="col" @click="goMain">
                 <img class="ft_img" src="../assets/img/whitehome.png" alt="no home">
             </div>
-            <div class="col">
+            <div class="col" @click="goDelivery">
                 <img class="ft_img" src="../assets/img/deliver_white.png" width="20%" alt="no home">
             </div>
-            <div class="col">
+            <div class="col" @click="goVoice">
                 <img class="ft_img" src="../assets/img/mic_white.png" width="20%" alt="no home">
             </div>
-            <div class="col">
+            <div class="col" @click="goControl">
                 <img class="ft_img" src="../assets/img/turn-off_white.png" width="20%" alt="no home">
             </div>
-            <div class="col">
+            <div class="col" @click="goMypage">
                 <img class="ft_img" src="../assets/img/user_white.png" width="20%" alt="no home">
             </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        goMain(){
+            this.$router.push('/main');
+        },
+        goDelivery(){
+            this.$router.push('/delivery');
+        },
+        goVoice(){
+            this.$router.push('/voice');
+        },
+        goControl(){
+            this.$router.push('/control');
+        },
+        goMypage(){
+            this.$router.push('/mypage');
+        }
+    },
+}
+</script>
+
+
 <style>
 .ft_img {
     width: 100%;
