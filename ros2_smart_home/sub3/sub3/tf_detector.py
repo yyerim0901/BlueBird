8d7b32067f56509ed1a8e2e18eb61470ef0ce126
이용직 <yj_7679@naver.com>
2021-10-03 22:06:47 +0900

    tf ing

--- ros2_smart_home/sub3/sub3/tf_detector.py
+++ ros2_smart_home/sub3/sub3/tf_detector.py
@@ -22,7 +22,7 @@ import os
 from rclpy.node import Node
 import time
 from sensor_msgs.msg import CompressedImage, LaserScan
-from ssafy_msgs.msg import BBox
+from ssafy_msgs.msg import BBox,TurtlebotStatus
 from std_msgs.msg import Int16,Int8
 
 import tensorflow.compat.v1 as tf
@@ -158,29 +158,36 @@ def visualize_images(image_out, t_cost):
     cv2.imshow(winname, cv2.resize(image_out, (2*image_out.shape[1], 2*image_out.shape[0])))
     cv2.waitKey(1)
 
-def working_status_callback(self,msg):
+def working_status_callback(msg):
     global working_status_msg
+    global is_working_status
+    is_working_status =True
     working_status_msg = msg
 
-def want_stuff_callback(self,msg):
+def want_stuff_callback(msg):
     global want_stuff_msg
+    global is_want_stuff
+    is_want_stuff = True
     want_stuff_msg = msg     
 
 def img_callback(msg):
 
     global img_bgr
-
+    global is_img_bgr
+    is_img_bgr =True
+    print("ccc")
     np_arr = np.frombuffer(msg.data, np.uint8)
     img_bgr = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
 def status_callback(msg):
     global turtlebot_status_msg
-
     turtlebot_status_msg = msg
 
 def scan_callback(msg):
 
-    global xyz
+    global xyz  
+    global is_scan
+    is_scan = True
 
     R = np.array(msg.ranges)
 
@@ -193,7 +200,12 @@ def scan_callback(msg):
         y.reshape([-1, 1]),
         z.reshape([-1, 1])
     ], axis=1)
-   
+
+def pub_timer():
+    global working_status_pub
+    global working_status_msg
+    global g_node
+    working_status_pub.publish(working_status_msg)
 
 def main(args=None):
 
@@ -233,6 +245,8 @@ def main(args=None):
     # 로직 3. detection model graph 생성
     # tf.Graph()를 하나 생성하고, 이전에 불러들인 pretrained file 안의 뉴럴넷 파라메터들을
     # 생성된 그래프 안에 덮어씌웁니다.    
+        # 로직 1. 제어 주기 및 타이머 설정
+
 
     detection_graph = tf.Graph()
     with detection_graph.as_default():
@@ -253,6 +267,8 @@ def main(args=None):
     config.gpu_options.allow_growth = True
     config.gpu_options.allocator_type = 'BFC'
 
+    global is_working_status
+    is_working_status =False
     # 로직 5. session 생성
     # 위에 정의한 graph와 config로 세션을 생성합니다.
     sess2 = tf.Session(graph=detection_graph, config=config)
@@ -269,6 +285,16 @@ def main(args=None):
     # inference를 하기 위함입니다    
 
     global g_node
+    global working_status_pub
+    global working_status_msg
+    global turtlebot_status_msg
+    global want_stuff_msg
+    global is_img_bgr
+    is_img_bgr = False
+    global is_scan
+    is_scan = False
+    global is_want_stuff
+    is_want_stuff = False
 
     rclpy.init(args=args)
 
@@ -276,22 +302,31 @@ def main(args=None):
     
 
     cmd_pub = g_node.create_publisher(Twist, 'cmd_vel', 10)
-    subscription_img = g_node.create_subscription(CompressedImage, '/image_jpeg/compressed', img_callback, 10)
     # status_sub = g_node.create_subscription(TurtlebotStatus,'/turtlebot_status',status_callback,10)
+    
+    # 로봇 절대위치 좌표
+    subscription = g_node.create_subscription(TurtlebotStatus, '/turtlebot_status',status_callback, 10)
 
     working_status_sub = g_node.create_subscription(Int16,'/working_status',working_status_callback,10) # woring status 값 받고 값을 다시 담아주기 위해 사용
+    working_status_pub = g_node.create_publisher(Int16,'working_status',10)
     want_stuff_sub = g_node.create_subscription(Int8,'/want_stuff', want_stuff_callback, 10) # 찾으려는 물건 값을 받기 위해 사용
     subscription_scan = g_node.create_subscription(LaserScan, '/scan', scan_callback, 3)
+    subscription_img = g_node.create_subscription(CompressedImage, '/image_jpeg/compressed', img_callback, 10)
 
-    #cmd_msg=Twist()
-
+    # 로봇 제어와 working status pub하는 타이머
+    time_period=0.05 
+    timer = g_node.create_timer(time_period, pub_timer)    
+    cmd_msg=Twist()
+    # 절대 좌표 받기위함
+    turtlebot_status_msg = TurtlebotStatus()
     # subscription_scan
 
     # subscription_img
     
     # 로직 8. lidar2img 좌표 변환 클래스 정의
     # sub2의 좌표 변환 클래스를 가져와서 정의.
-
+    working_status_msg = Int16()
+    want_stuff_msg =Int8()
     l2c_trans = LIDAR2CAMTransform(params_cam, params_lidar)
 
     iter_step = 0
@@ -299,105 +334,118 @@ def main(args=None):
     while rclpy.ok():
 
         time.sleep(0.05)
-        
+
         # 로직 9. ros 통신을 통한 이미지 수신
         for _ in range(2):
 
             rclpy.spin_once(g_node)
+        if is_img_bgr and is_working_status and is_scan and is_want_stuff:
+            # 터틀 봇 위치 받음
+            loc_x = turtlebot_status_msg.twist.angular.x
+            loc_y = turtlebot_status_msg.twist.angular.y
+            # 로직 10. object detection model inference
+            image_process, infer_time, boxes_detect, scores, classes_pick = ssd_net.inference(img_bgr)
 
-        # 로직 10. object detection model inference
-        image_process, infer_time, boxes_detect, scores, classes_pick = ssd_net.inference(img_bgr)
+            # 로직 11. 라이다-카메라 좌표 변환 및 정사영
+            # sub2 에서 ex_calib 에 했던 대로 라이다 포인트들을
+            # 이미지 프레임 안에 정사영시킵니다.
+            
+            # 전방 라이다만 뽑음
+            xyz_p = xyz[np.where(xyz[:, 0]>=0)]
 
-        # 로직 11. 라이다-카메라 좌표 변환 및 정사영
-        # sub2 에서 ex_calib 에 했던 대로 라이다 포인트들을
-        # 이미지 프레임 안에 정사영시킵니다.
+            xyz_c = l2c_trans.transform_lidar2cam(xyz_p)
+            #print('xyz_c')
+            #print(xyz_c)
+            xy_i = l2c_trans.project_pts2img(xyz_c, False)
+            #print('xy_i')
+            #print(xy_i)
+            # xyii = np.concatenate([xy_i, xyz_p], axis=1)
+            # xyz_c 가 카메라에서 바라본 라이다의 x,y,z 좌 표
+            xyii = np.concatenate([xy_i, xyz_p], axis=1)
+            #print('xyii')
+            #print(xyii)
+            """
 
-        xyz_p = xyz[np.where(xyz[:, 0]>=0)]
+            # 로직 12. bounding box 결과 좌표 뽑기
+            ## boxes_detect 안에 들어가 있는 bounding box 결과들을
+            ## 좌상단 x,y와 너비 높이인 w,h 구하고, 
+            ## 본래 이미지 비율에 맞춰서 integer로 만들어
+            ## numpy array로 변환
 
-        xyz_c = l2c_trans.transform_lidar2cam(xyz_p)
+            """
+            # 인식되는 객체들중에 원하는게 있는지 확인.
+            is_find_object = False
+            idx = 0
+            for value in classes_pick[0] :
+                idx = idx + 1
+                if((int)(value) == want_stuff_msg.data):
+                    is_find_object = True
+                    print(value, "찾음")
 
-        xy_i = l2c_trans.project_pts2img(xyz_c, False)
+                    if len(boxes_detect) != 0:
+                        ih = img_bgr.shape[0]
+                        iw = img_bgr.shape[1]
+                        print(ih)
+                        boxes_np = np.array(boxes_detect[0])
+                        x = boxes_np.T[1] * iw
+                        y = boxes_np.T[0] * ih
+                        w = (boxes_np.T[3] - boxes_np.T[1]) * iw
+                        h = (boxes_np.T[2] - boxes_np.T[0]) * ih
+                        bbox = np.vstack([
+                            x.astype(np.int32).tolist(),
+                            y.astype(np.int32).tolist(),
+                            w.astype(np.int32).tolist(),
+                            h.astype(np.int32).tolist()
+                        ]).T
+                        print("bbox")
+                        print(bbox)
+                        
+                        x = int(bbox[0, 0])
+                        y = int(bbox[0, 1])
+                        w = int(bbox[0, 2])
+                        h = int(bbox[0, 3])
 
-        xyii = np.concatenate([xy_i, xyz_p], axis=1)
-        # print(xyii)
+                        cx = int((x + w) / 2)
+                        cy = int((y + h) / 2)
+                        
+                        xyv = xyii[np.logical_and(xyii[:, 0]>=x, xyii[:, 0]<=x+w), :]
+                        xyv = xyv[np.logical_and(xyv[:, 1]>=y, xyv[:, 1]<=y+h), :]
+                        relative_x = xyv[2]
+                        relative_y = xyz[3]
+                        relative_z = xyz[4]
+                        
+                        relative = np.array([relative_x, relative_y,relative_z])    
+                        #     xyv = 
+                        # print(xyv)
+                        ## bbox 안에 들어가는 라이다 포인트들의 대표값(예:평균)을 뽑는다
+                        ostate = np.median(xyv, axis=0)
+                        print(ostate)
+                # print(is_find_object)    
 
-        """
+                # 로직 13. 인식된 물체의 위치 추정
+                ## bbox가 구해졌으면, bbox 안에 들어가는 라이다 포인트 들을 구하고
+                ## 그걸로 물체의 거리를 추정할 수 있습니다.
+                
 
-        # 로직 12. bounding box 결과 좌표 뽑기
-        ## boxes_detect 안에 들어가 있는 bounding box 결과들을
-        ## 좌상단 x,y와 너비 높이인 w,h 구하고, 
-        ## 본래 이미지 비율에 맞춰서 integer로 만들어
-        ## numpy array로 변환
 
-        """
-        # 인식되는 객체들중에 원하는게 있는지 확인.
-        is_find_object = False
-        for i in classes_pick[0] :
-            if((int)(i) == want_stuff_msg.data):
-            is_find_object = True
  
-        if len(boxes_detect) != 0:
 
-            ih = img_bgr.shape[0]
-            iw = img_bgr.shape[1]
-
-            boxes_np = np.array(boxes_detect)
-
-            x = boxes_np.T[0] * iw
-            y = boxes_np.T[1] * ih
-            w = (boxes_np.T[2] - boxes_np.T[0]) * iw
-            h = (boxes_np.T[3] - boxes_np.T[1]) * ih
-
-            bbox = np.vstack([
-                x.astype(np.int32).tolist(),
-                y.astype(np.int32).tolist(),
-                w.astype(np.int32).tolist(),
-                h.astype(np.int32).tolist()
-            ]).T
-
-            # print(bbox)
-
-
-
-            # 로직 13. 인식된 물체의 위치 추정
-            ## bbox가 구해졌으면, bbox 안에 들어가는 라이다 포인트 들을 구하고
-            ## 그걸로 물체의 거리를 추정할 수 있습니다.
-            
-            ostate_list = []
-
-            for i in range(bbox.shape[0]):
-                x = int(bbox[i, 0])
-                y = int(bbox[i, 1])
-                w = int(bbox[i, 2])
-                h = int(bbox[i, 3])
-
-                cx = (x + w) // 2
-                cy = (y + h) // 2
+                image_process = draw_pts_img(image_process, xy_i[:, 0].astype(np.int32),
+                                                xy_i[:, 1].astype(np.int32))
                 
-                xyv = xyii[np.logical_and(xyii[:, 0]>=cx-0.4*w, xyii[:, 0]<cx+0.4*w), :]
-                xyv = xyv[np.logical_and(xyv[:, 1]>=cy-0.4*h, xyv[:, 1]<cy+0.4*h), :]
-                
-            #     xyv = 
 
-                ## bbox 안에 들어가는 라이다 포인트들의 대표값(예:평균)을 뽑는다
-                ostate = np.median(xyv, axis=0)
-                # print(ostate)
+            visualize_images(image_process, infer_time)
+            print(is_working_status)
+            # can_find_object(0b0111)일 때, 심부름에서 회전하며 물건찾기 - object 못찾은 경우만 터틀 봇이 돌아감 -> doing_find_object(0b1111)
+            # if is_working_status and working_status_msg.data == 0b0111:
+            #     working_status_msg.data = 0b1111
+            #     cmd_msg.linear.x = 0.0
+            #     cmd_msg.angular.z = 0.5
+            #     cmd_pub.publish(cmd_msg)
 
-                ## 대표값이 존재하면 
-                if not np.isnan(ostate[0]):
-                    ostate_list.append(ostate)
+            # # doing_find 하다가 찾은 경우 목적지 찾기
+            # if is_find_object and working_status_msg.data = 0b1111
 
-            image_process = draw_pts_img(image_process, xy_i[:, 0].astype(np.int32),
-                                            xy_i[:, 1].astype(np.int32))
-            
-            print(ostate_list)
-        visualize_images(image_process, infer_time)
-
-        #심부름에서 회전하며 물건찾기 - object 못찾은 경우만 터틀 봇이 돌아감
-        if working_status_msg.data == 0b0111 :
-            cmd_msg.linear.x = 0.0
-            cmd_msg.angular.z = 0.5
-            cmd_pub.publish(cmd_msg)
 
     g_node.destroy_node()
     rclpy.shutdown()
