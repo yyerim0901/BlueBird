13722e0489035b4d55b1a6fbe78abd81f59fd314
ParkMinsang <whsrpdla@naver.com>
2021-10-05 19:45:07 +0900

    modules add

---
+++ ros2_smart_home/sub3/sub3/modules.py
@@ -0,0 +1,17 @@
+import math
+
+def checkCurrStage(stage):
+    ret = 0
+    while(stage%2!=0):
+        ret+=1
+        stage = int(stage/2)
+    return ret
+
+def getCurrStage(stageNum):
+    ret = 0
+    idx = 0
+    while(stageNum>0):
+        ret += int(math.pow(2, idx))
+        idx+=1
+        stageNum-=1
+    return ret
